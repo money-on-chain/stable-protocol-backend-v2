d5bce96cb9955a365042bf448921668a4076c34b
mulonemartin <mulone.martin@gmail.com>
2024-01-24 14:00:39 -0300

    feat: queue params

---
+++ scripts/admin/queue-params.js
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv'
+
+import { readJsonFile, getWeb3 } from '../../src/utils.js'
+import { readContracts } from '../../src/moc-v2/contracts.js'
+import { QueueExecute } from '../../src/moc-v2/admin.js'
+
+dotenv.config()
+
+const main = async () => {
+    const configPath = './settings/projects.json'
+    const configProject = readJsonFile(configPath).projects[process.env.MOC_PROJECT.toLowerCase()]
+
+    // get web3 connection
+    const web3 = getWeb3(process.env.HOST_URI)
+
+    // Obtain all contracts
+    const dContracts = await readContracts(web3, configProject)
+    const MocQueue = dContracts.contracts.MocQueue
+
+    const params = await MocQueue.methods.operationsMintTC(2).call()
+
+    console.log(params)
+}
+
+main()
